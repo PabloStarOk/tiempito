2eca98b146dc0f7f092983ae909746751b47bab1
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2024-12-26 15:35:40 -0500

    feat(daemon-config): add config for notification manager

--- src/daemon/Tiempitod.NET/Configuration/NotificationConfig.cs
+++ src/daemon/Tiempitod.NET/Configuration/NotificationConfig.cs
@@ -3,8 +3,10 @@ namespace Tiempitod.NET.Configuration;
 public class NotificationConfig
 {
     public const string Notification = "Notification";
-    
-    public int ExpirationTimeoutMs { get; set; }
+
+    public string AppName { get; set; } = "Tiempito";
+    public string IconPath { get; set; } = string.Empty;
+    public int ExpirationTimeoutMs { get; set; } = 10000;
     public string SessionStartedSummary { get; set; } = "Session started";
     public string SessionStartedBody { get; set; } = "A new session was started.";
     public string SessionFinishedSummary { get; set; } = "Session finished";
